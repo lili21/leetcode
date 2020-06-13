ae865d08acdd5d1fbd77dab0d61b02de21302c9a
li.li <mark.li210812@gmail.com>
2020-06-13 11:58:54 +0800

    最长回文子串

--- src/longestPalindrome.js
+++ src/longestPalindrome.js
@@ -1,31 +1,32 @@
 // https://leetcode-cn.com/problems/longest-palindromic-substring/solution/zui-chang-hui-wen-zi-chuan-by-leetcode-solution/
-var longestPalindrome = function (s) {
-  let res = s[0] || ''
-  for (let i = 0; i < s.length; i++) {
-    const c = s[i]
-    const indexs = findAllIndex(s, c)
-    for (let j = 0; j < indexs.length; j++) {
-      const substring = s.slice(i, indexs[j] + 1)
-      if (isPalindrome(substring)) {
-        res = substring.length > res.length ? substring : res
+function longestPalindrome (s) {
+  const n = s.length
+  if (n < 2) {
+    return s
+  }
+  const pd = new Array(n).fill(false).map(() => new Array(n).fill(false))
+  let anx = ''
+  for (let l = 0; l < n; l++) {
+    pd[l][l] = true
+  }
+
+  for (let r = 1; r < n; r++) {
+    for (let l = 0; l < r; l++) {
+      if (s[l] !== s[r]) {
+        pd[l][r] = false
+      } else {
+        if (r - l < 3) {
+          pd[l][r] = true
+        } else {
+          pd[l][r] = pd[l + 1][r - 1]
+        }
+      }
+      if (pd[l][r] && r - l + 1 > anx.length) {
+        anx = s.slice(l, r + 1)
       }
     }
   }
-  return res
+  return anx
 }
 
-function findAllIndex (s, v) {
-  const indexs = []
-  let i = s.indexOf(v)
-  while (i !== -1) {
-    indexs.push(i)
-    i = s.indexOf(v, i + 1)
-  }
-  return indexs
-}
-
-function isPalindrome (s) {
-  return s === s.split('').reverse().join('')
-}
-
-console.log(longestPalindrome('bababcd'))
+console.log(longestPalindrome('ab'))
