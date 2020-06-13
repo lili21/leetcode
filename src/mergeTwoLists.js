417389f4c9cb62810c8f92c28e6c7b926b7e1f4c
li.li <mark.li210812@gmail.com>
2020-06-13 11:51:14 +0800

    链表合并

---
+++ src/mergeTwoLists.js
@@ -0,0 +1,24 @@
+// https://leetcode-cn.com/explore/featured/card/bytedance/244/linked-list-and-tree/1048/
+import { ListNode } from './utils'
+
+const l1 = new ListNode(1)
+l1.next = new ListNode(2)
+l1.next.next = new ListNode(3)
+
+const l2 = new ListNode(1)
+l2.next = new ListNode(3)
+l2.next.next = new ListNode(4)
+
+function mergeTwoLists (l1, l2) {
+  if (!l1) return l2
+  if (!l2) return l1
+  if (l1.val < l2.val) {
+    l1.next = mergeTwoLists(l1.next, l2)
+    return l1
+  } else {
+    l2.next = mergeTwoLists(l2.next, l1)
+    return l2
+  }
+}
+
+console.log(mergeTwoLists(l1, l2).toString())
